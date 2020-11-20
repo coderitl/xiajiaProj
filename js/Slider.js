0c9304cfef8d97413a32ae0ab84889bd31ecd907
wang_hong_bin <3327511395@qq.com>
2020-11-20 23:20:11 +0800

    Finish 2020-11-21 Over

--- js/Slider.js
+++ js/Slider.js
@@ -5,59 +5,59 @@ const prev = document.querySelector("#prev");
 const next = document.querySelector("#next");
 
 const nextSlider = function () {
-  // get current class
-  const current = document.querySelector(".current");
-  if (current.nextElementSibling) {
-    // add current to next sibling
-    current.nextElementSibling.classList.add("current");
-  } else {
-    // add current to start
-    sliders[0].classList.add("current");
-  }
-  // clear current
-  setTimeout(() => current.classList.remove("current"));
+    // get current class
+    const current = document.querySelector(".current");
+    if (current.nextElementSibling) {
+        // add current to next sibling
+        current.nextElementSibling.classList.add("current");
+    } else {
+        // add current to start
+        sliders[0].classList.add("current");
+    }
+    // clear current
+    setTimeout(() => current.classList.remove("current"));
 };
 
 const prevSlider = function () {
-  // get current class
-  const current = document.querySelector(".current");
-  if (current.previousElementSibling) {
-    // add current to next sibling
-    current.previousElementSibling.classList.add("current");
-  } else {
-    // add current to start
-    sliders[sliders.length - 1].classList.add("current");
-  }
-  // clear current
-  setTimeout(() => current.classList.remove("current"));
+    // get current class
+    const current = document.querySelector(".current");
+    if (current.previousElementSibling) {
+        // add current to next sibling
+        current.previousElementSibling.classList.add("current");
+    } else {
+        // add current to start
+        sliders[sliders.length - 1].classList.add("current");
+    }
+    // clear current
+    setTimeout(() => current.classList.remove("current"));
 };
 
 // click Event
 next.addEventListener("click", function () {
-  nextSlider();
+    nextSlider();
 });
 prev.addEventListener("click", function () {
-  prevSlider();
+    prevSlider();
 });
 
 // 定时器
 var timer = setInterval(function () {
-  nextSlider();
+    nextSlider();
 }, 5000);
 
 // 鼠标移入移除
 var rmTimer = document.querySelector("#Slider ");
 rmTimer.addEventListener("mouseenter", function () {
-  // 鼠标移入清除 定时器
-  clearInterval(timer);
-  // 清除定时器变量
-  timer = null;
-  console.log("鼠标移入,清除定时器···");
+    // 鼠标移入清除 定时器
+    clearInterval(timer);
+    // 清除定时器变量
+    timer = null;
+    console.log("鼠标移入,清除定时器···");
 });
 rmTimer.addEventListener("mouseleave", function () {
-  // 鼠标移出开启定时器
-  timer = setInterval(function () {
-    nextSlider();
-  }, 2000);
-  console.log("鼠标移入,开启定时器···");
+    // 鼠标移出开启定时器
+    timer = setInterval(function () {
+        nextSlider();
+    }, 2000);
+    console.log("鼠标移入,开启定时器···");
 });
