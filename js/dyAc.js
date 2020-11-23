a7f324040a32c9e3b95561fb73e133e0c489cc07
wang_hong_bin <3327511395@qq.com>
2020-11-23 16:27:03 +0800

    finish: 2020-11-23 Subpage dyAc.html

--- js/dyAc.js
+++ js/dyAc.js
@@ -1,16 +1,13 @@
-// 获取元素
-const slider = document.querySelector(".slider");
-const acNav = document.querySelector(".ac-nav");
-// 解决闪烁问题
-var hiddenBox = document.querySelector(".hiddenBox");
-// 获取 slider 的 offsetTop 高度
-const sliderTop = slider.offsetTop;
-document.addEventListener("scroll", function () {
-  if (window.pageYOffset >= sliderTop) {
-    acNav.style.position = "fixed";
-    hiddenBox.style.display = "block";
-  } else {
-    acNav.style.position = "absolute";
-    hiddenBox.style.display = "none";
-  }
-});
+var isFixed = false;
+var acHeader = document.querySelector("#ac-header");
+window.onscroll = function () {
+    // https://caniuse.com/?search=pageYOffset 兼容查看
+    // 滚动条兼容处理
+    var scrollTop = window.pageYOffset || document.documentElement.offsetTop || document.body.offsetTop;
+    isFixed = scrollTop > 100 ? true : false;
+
+    isFixed
+        ? acHeader.className = "acSticky"
+        : acHeader.className = "";
+// 取消
+}
